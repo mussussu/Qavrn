981597314192e653681bbbeb4368cfd4b8e63725
mussussu <jorge3687@gmail.com>
2026-03-16 09:22:20 -0700

    Remove test docs from repo, add to gitignore

--- test_docs/bank.py
+++
@@ -1,8 +0,0 @@
-prompt = input("type a greeting ").strip().casefold()
-if prompt.startswith("hello"):
-    print("$0")
-elif prompt.startswith("h"):
-    if prompt != "hello":
-        print("$20")
-else:
-    print("$100")
